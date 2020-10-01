82add7ad707fe1c1216750e161192bb1e150cfd6
MayCV <maycvd00749@fpt.edu.vn>
2020-10-01 11:34:48 +0700

    model commit

--- app/User.php
+++ app/User.php
@@ -2,9 +2,8 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -27,8 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
+     /**
      * The attributes that should be cast to native types.
      *
      * @var array
@@ -36,4 +34,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function role(){
+        return $this->belongsTo(Role::class, 'id_role','id_role');
+    }
+
+    public function staff(){
+        return $this->belongsTo('App\Staff', 'id', 'id');
+    }
+
+    // hàm kiểm tra user hiện tại có được gán 1 quyền nào đó hay không,
+    // nếu có thì trả về true
+    public function hasPermission(Permission $permission){
+//        echo $permission->name;
+
+        $check = !!optional(optional($this->role)->permission)->contains($permission);
+//        var_dump($check);
+//        die();
+        return $check;
+    }
 }
